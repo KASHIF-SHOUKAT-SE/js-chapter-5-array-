1ba6c655b78759ba59f2d1a0c79f7cbc77be0c59
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-28 11:13:04 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// let marks = [23,45,65,87,98]
+// marks[5] = 76;
+// console.log(marks)
+// console.log(marks[0])
+// console.log(marks[1])
+// console.log(marks[2])
+// console.log(marks[5])
+// marks[0] = 99;
+// console.log(marks)
+
+// for(let a=0; a<marks.length; a++){
+//     console.log(marks[a])
+// }
+
+// let a = [34,65,78,89]
+// let b =a.toString()
+// console.log(b)
+// let c = a.join("_")
+// console.log(c)
+// let r = a.pop()
+// console.log(a,r)
+// let r = a.push(49)
+// console.log(a,r)
+
+// let r = a.shift() // remove element from the start of arry
+// console.log(r,a)
+// let r = a.unshift(22)
+// console.log(r,a)
+
+// let num = [1,2,3,4,5,6,7,8,9]
+// let num1 =[9,8,7,6,5,4,3,2,1]
+// delete num[0]
+// console.log(num)
+// let newarray = num.concat(num1)
+// console.log(newarray);
+
+// let num = [65,87,90,34,54]
+// num.sort()
+// console.log(num)
+// num.reverse()
+// console.log(num)
+
+// num.splice(2,4,767,876,543,567)
+// console.log(num)
+
+// let newnum = num.slice(3);
+// console.log(newnum)
+
+// let num = [3,4,5,6,7,8]
+// for(i=0; i<num.length; i++){
+//     console.log(num[i])
+// }
+
+let array = "kashif";
+let name1 =Array.from(array)
+console.log(name1)
+
+
+
